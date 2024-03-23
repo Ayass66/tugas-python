01d79f14358dead102216af69b47532cb2a575c8
Ayass66 <145944946+Ayass66@users.noreply.github.com>
2024-03-23 15:02:44 +0800

    Add files via upload
    Tugas Struktur data, Algoritma dan  Kompleksitas.

---
+++ tugas1.py
@@ -0,0 +1,16 @@
+#buat program untuk menampilkan biodata
+
+
+nama=("Anggi Larasati")
+nim=("SI20230003")
+umur=int(18)
+alamat=("Darmaji")
+lulusan=("SMAN 2 Praya")
+
+print("=="*10)
+print("selamat datang", nama)
+print("nim anda", nim)
+print("umur anda", umur)
+print("alamat anda", alamat)
+print("lulusan sekolah", lulusan)
+print("=="*10)
